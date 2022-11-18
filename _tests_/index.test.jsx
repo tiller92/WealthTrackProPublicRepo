8ca1c54df41fed4c1a2e7dd98897c033c268a2c5
tiller92 <ryantiller6@gmail.com>
2022-11-18 17:46:44 -0500

    test setup almost there

---
+++ _tests_/index.test.jsx
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react'
+import {Home} from '../pages/index'
+import GetStarted from '../components/GetStarted'
+import '@testing-library/jest-dom'
+
+describe('Home', () => {
+	  it('renders a heading', () => {
+		      render(<GetStarted />)
+
+		      const heading = screen.getByRole('heading', {
+			            name: /welcome to next\.js!/i,
+			          })
+
+		      expect(heading).toBeInTheDocument()
+		    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
